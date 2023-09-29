d41bb3bc5c1b66cbc0d1a242b4b2778c7eb7e205
tosaken1116 <nagato20021122@gmail.com>
2023-09-29 15:10:01 +0900

    fix: add query param validator

--- src/libs/fetchQuery/index.test.ts
+++ src/libs/fetchQuery/index.test.ts
@@ -1,3 +1,5 @@
+import { object, string } from 'zod';
+
 import { fetchQuery } from '.';
 
 describe('fetchQuery libs', () => {
@@ -15,5 +17,31 @@ describe('fetchQuery libs', () => {
 
     const result = fetchQuery(url, params);
     expect(result).toBe('http://example.com?');
+  });
+
+  it('バリデーションスキーマがある時に正しいクエリパラメータを渡すとパラメータ付きでurlを返す', () => {
+    const testSchema = object({
+      name: string(),
+    });
+
+    const url = 'http://example.com';
+
+    const params = new URLSearchParams({ name: 'user' });
+
+    const result = fetchQuery(url, params, testSchema);
+    expect(result).toBe('http://example.com?name=user');
+  });
+
+  it('バリデーションスキーマがある時に間違ったクエリパラメータを渡すとエラーを返す', () => {
+    const testSchema = object({
+      name: string(),
+    });
+
+    const url = 'http://example.com';
+    const params = new URLSearchParams({ number: '123' });
+
+    expect(() => fetchQuery(url, params, testSchema)).toThrowError(
+      'Data validation failed'
+    );
   });
 });
--- src/libs/fetchQuery/index.ts
+++ src/libs/fetchQuery/index.ts
@@ -1,4 +1,18 @@
-export const fetchQuery = (url: string, params: URLSearchParams): string => {
-  const query = new URLSearchParams(params);
-  return `${url}?${query.toString()}`;
+import type { ZodSchema } from 'zod';
+
+export const fetchQuery = (
+  url: string,
+  params: URLSearchParams,
+  schema?: ZodSchema
+): string => {
+  if (!schema) {
+    return `${url}?${params.toString()}`;
+  }
+  const paramsObj = Object.fromEntries(params.entries());
+
+  const parsed = schema.safeParse(paramsObj);
+  if (!parsed.success) {
+    throw new Error('Data validation failed');
+  }
+  return `${url}?${params.toString()}`;
 };
