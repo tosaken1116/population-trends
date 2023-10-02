ed63a252f611238cc6ba499f7795dd58af592e7d
tosaken1116 <nagato20021122@gmail.com>
2023-10-02 17:53:18 +0900

    fix: use hooks inside presentation

--- src/components/model/PrefectureCheckBoxes/presentations/index.tsx
+++ src/components/model/PrefectureCheckBoxes/presentations/index.tsx
@@ -1,36 +1,31 @@
-import { container, prefectureItem, selectedItem } from './css/index.css';
+import { usePrefectureCheckBoxes } from '../hooks';
 
-import type { Response } from '@/app/api/v1/prefectures/schema';
+import { container, prefectureItem, selectedItem } from './css/index.css';
 
 import { CheckBox } from '@/components/ui/CheckBox';
 
-type Props = {
-  onChange: (target: Response['result'][number]) => void;
-  prefectures: Response['result'];
-  selectedPrefectures: Response['result'];
-};
+export const PrefectureCheckBoxesPresentation: React.FC = () => {
+  const { onChange, prefectures, selectedPrefectures } =
+    usePrefectureCheckBoxes();
 
-export const PrefectureCheckBoxesPresentation: React.FC<Props> = ({
-  onChange,
-  selectedPrefectures,
-  prefectures,
-}) => (
-  <ul className={container}>
-    {prefectures.map((prefecture) => {
-      const selected = selectedPrefectures.includes(prefecture);
-      return (
-        <li key={prefecture.prefCode} className={prefectureItem}>
-          <CheckBox
-            checked={selected}
-            labelText={prefecture.prefName}
-            onChange={(): void => {
-              onChange(prefecture);
-            }}
-            className={selected ? selectedItem : ''}
-            size="m"
-          />
-        </li>
-      );
-    })}
-  </ul>
-);
+  return (
+    <ul className={container}>
+      {prefectures?.map((prefecture) => {
+        const selected = selectedPrefectures.includes(prefecture);
+        return (
+          <li key={prefecture.prefCode} className={prefectureItem}>
+            <CheckBox
+              checked={selected}
+              labelText={prefecture.prefName}
+              onChange={(): void => {
+                onChange(prefecture);
+              }}
+              className={selected ? selectedItem : ''}
+              size="m"
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
