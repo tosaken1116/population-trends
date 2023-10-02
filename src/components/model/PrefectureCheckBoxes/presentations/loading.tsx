928896e8db529ef5cc4c0da5feeb364dd3eb9c11
tosaken1116 <nagato20021122@gmail.com>
2023-10-02 14:32:36 +0900

    feat: add checkboxes loading presentation

---
+++ src/components/model/PrefectureCheckBoxes/presentations/loading.tsx
@@ -0,0 +1,23 @@
+import {
+  checkboxStyle,
+  container,
+  labelStyle,
+  prefectureItem,
+} from './css/loading.css';
+
+import { Skeleton } from '@/components/ui/Skeleton';
+
+export const PrefectureCheckBoxesLoadingPresentation: React.FC = () => (
+  <ul className={container}>
+    {[...Array<number>(47)].map((_: number, i: number) => (
+      <li key={i} className={prefectureItem}>
+        <div className={checkboxStyle}>
+          <Skeleton width={24} height={24} rounded="m" />
+        </div>
+        <div className={labelStyle}>
+          <Skeleton width={64} height={24} rounded="m" />
+        </div>
+      </li>
+    ))}
+  </ul>
+);
