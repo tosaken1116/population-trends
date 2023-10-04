195dd3e741b2826394ef8ce5bca6a792ff7c464b
tosaken1116 <nagato20021122@gmail.com>
2023-10-05 03:00:25 +0900

    feat: add Population Chart Loading Presentation

---
+++ src/components/model/PopulationChart/presentations/loading.tsx
@@ -0,0 +1,13 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { container } from './css/base.css';
+
+import { Typography } from '@/components/ui/Typography';
+
+export const PopulationChartLoadingPresentation: React.FC = () => (
+  <div className={container}>
+    <FontAwesomeIcon size="3x" icon={faSpinner} spin />
+    <Typography variant="h1">データを取得中...</Typography>
+  </div>
+);
