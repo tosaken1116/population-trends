ff0dcbc349dfd77a895481b5be27163b12d72ff7
tosaken1116 <nagato20021122@gmail.com>
2023-10-05 02:59:50 +0900

    feat: add Population Chart Error Presentation

---
+++ src/components/model/PopulationChart/presentations/error.tsx
@@ -0,0 +1,21 @@
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { container } from './css/base.css';
+import { errorIcon, errorText } from './css/error.css';
+
+import { Typography } from '@/components/ui/Typography';
+
+export const PopulationChartErrorPresentation: React.FC = () => (
+  <div className={container}>
+    <FontAwesomeIcon
+      icon={faCircleExclamation}
+      shake
+      size="2x"
+      className={errorIcon}
+    />
+    <Typography variant="h1" className={errorText}>
+      都道府県の取得中にエラーが発生しました
+    </Typography>
+  </div>
+);
