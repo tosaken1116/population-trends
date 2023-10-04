9c0a88976777207aa14b0d569deb4fa897c83361
tosaken1116 <nagato20021122@gmail.com>
2023-10-05 02:58:47 +0900

    feat: add Population Chart presentation

---
+++ src/components/model/PopulationChart/presentations/index.tsx
@@ -0,0 +1,62 @@
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+
+import { usePopulationChart } from '../hooks';
+
+import { container } from './css/base.css';
+import { PopulationChartEmptyPresentation } from './empty';
+
+import type { VisibleData } from '../hooks';
+
+type Props = {
+  data: VisibleData;
+};
+
+export const PopulationChartPresentation: React.FC<Props> = ({ data }) => (
+  <div className={container}>
+    <ResponsiveContainer width="99%" height="80%">
+      <LineChart
+        width={1000}
+        height={1000}
+        margin={{
+          top: 5,
+          right: 5,
+          left: 5,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <YAxis dataKey="value" />
+        <XAxis dataKey="year" type="category" allowDuplicatedCategory={false} />
+
+        <Legend />
+        <Tooltip />
+        {data.map(({ prefecture, data }) => (
+          <Line
+            key={prefecture}
+            dataKey="value"
+            data={data}
+            name={prefecture}
+          />
+        ))}
+      </LineChart>
+    </ResponsiveContainer>
+  </div>
+);
+
+export const PopulationChartContainer: React.FC = () => {
+  const { visibleData } = usePopulationChart();
+
+  if (visibleData == null || visibleData.length === 0) {
+    return <PopulationChartEmptyPresentation />;
+  }
+  return <PopulationChartPresentation data={visibleData} />;
+};
