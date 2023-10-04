0f20144327de2e3b2429b6cc637979e4cfdb01f9
tosaken1116 <nagato20021122@gmail.com>
2023-10-05 02:58:09 +0900

    feat: add PopulationChart Empty Presentation

---
+++ src/components/model/PopulationChart/presentations/empty.tsx
@@ -0,0 +1,10 @@
+import { container } from './css/base.css';
+
+import { Typography } from '@/components/ui/Typography';
+
+export const PopulationChartEmptyPresentation: React.FC = () => (
+  <div className={container}>
+    <Typography variant="h1">🍃</Typography>
+    <Typography variant="h1">データが選択されていません</Typography>
+  </div>
+);
