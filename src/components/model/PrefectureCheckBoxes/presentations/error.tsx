4b4ed988de217df02e900815248fff5f49f8925f
tosaken1116 <nagato20021122@gmail.com>
2023-10-05 14:39:53 +0900

    fix: prefectureCheckBoxesのスタイル修正

--- src/components/model/PrefectureCheckBoxes/presentations/error.tsx
+++ src/components/model/PrefectureCheckBoxes/presentations/error.tsx
@@ -1,20 +1,35 @@
 import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { container, errorIcon, errorText } from './css/error.css';
+import {
+  container,
+  errorIcon,
+  errorMessage,
+  errorText,
+  loadingContainer,
+  overlay,
+} from './css/error.css';
+import { PrefectureCheckBoxesLoadingPresentation } from './loading';
 
 import { Typography } from '@/components/ui/Typography';
 
 export const PrefectureCheckBoxesErrorPresentation: React.FC = () => (
   <div className={container} data-testid="error">
-    <FontAwesomeIcon
-      icon={faCircleExclamation}
-      shake
-      size="2x"
-      className={errorIcon}
-    />
-    <Typography className={errorText}>
-      都道府県の取得中にエラーが発生しました
-    </Typography>
+    <div className={loadingContainer}>
+      <PrefectureCheckBoxesLoadingPresentation />
+    </div>
+    <div className={overlay}>
+      <div className={errorMessage}>
+        <FontAwesomeIcon
+          icon={faCircleExclamation}
+          shake
+          size="2x"
+          className={errorIcon}
+        />
+        <Typography className={errorText}>
+          都道府県の取得中にエラーが発生しました
+        </Typography>
+      </div>
+    </div>
   </div>
 );
